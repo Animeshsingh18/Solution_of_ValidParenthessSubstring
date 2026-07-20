d7b58244666d9ad4a04bf4e2f9efdaa03b24f7f6
Animesh Singh <123716564+Animeshsingh18@users.noreply.github.com>
2026-07-20 17:59:21 +0530

    Optimized Two-Pass Approach.cpp

--- Optimized Two-Pass Approach.cpp
+++ Optimized Two-Pass Approach.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 int longestValidParentheses(string s) {
-    int left = 0, right = 0;
-    int maxLen = 0;
+    int left = 0, right = 0, maxLen = 0;
 
     for (char ch : s) {
         if (ch == '(')
@@ -14,11 +13,10 @@ int longestValidParentheses(string s) {
             right++;
 
         if (left == right)
-            maxLen = max(maxLen, 2 * right);
+            maxLen = max(maxLen,2 * left);
         else if (right > left)
             left = right = 0;
     }
-
     left = right = 0;
 
     for (int i = s.length() - 1; i >= 0; i--) {
@@ -28,20 +26,17 @@ int longestValidParentheses(string s) {
             right++;
 
         if (left == right)
-            maxLen = max(maxLen, 2 * left);
+          maxLen = max(maxLen, 2 * left);
         else if (left > right)
-            left = right = 0;
+          left = right = 0;
     }
-
     return maxLen;
 }
 
 int main() {
     string s;
     cin >> s;
-
     cout << longestValidParentheses(s) << endl;
-
     return 0;
 }
 
